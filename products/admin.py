a574a9887bb1017a89d1bda930fde7dbc770d8dd
Your Name <you@example.com>
2025-07-13 11:47:55 +0330

    some admin panel changing

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active',]
+    extra = 0
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', ]
+
+    inlines = [
+        CommentsInline,
+    ]
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'body', 'stars', 'active',]
