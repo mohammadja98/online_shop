9136b66f49826f49f4ea7349d74a5803689e42b5
Your Name <you@example.com>
2025-07-16 19:58:27 +0330

    richtext

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'rosetta',
     'jalali_date',
+    'ckeditor',
 
 
     'accounts',
@@ -168,6 +169,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 #media
 MEDIA_URL = '/media/'
